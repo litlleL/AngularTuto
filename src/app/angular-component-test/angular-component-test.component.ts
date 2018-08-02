a474eff9adcf4d941b06202ca8896fefad060b53
litlleL@litlleL-Pc <litlleL@litlleL-Pc>
2018-08-02 22:45:21 +0100

    Ajout nouveau angular-component-test

---
+++ src/app/angular-component-test/angular-component-test.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-angular-component-test',
+  templateUrl: './angular-component-test.component.html',
+  styleUrls: ['./angular-component-test.component.scss']
+})
+export class AngularComponentTestComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AngularComponentTestComponent } from './angular-component-test/angular-component-test.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AngularComponentTestComponent
   ],
   imports: [
     BrowserModule
